42cf3d80cbc50d772d81f47d9cf3d4236e3b4910
unknown <samuelilagan05@gmail.com>
2024-09-28 14:55:32 -0400

    fixed CORS issue in api_gateway module

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -79,4 +79,6 @@ resource "aws_api_gateway_integration_response" "cors_options_integration_respon
     "method.response.header.Access-Control-Allow-Methods"    = "'OPTIONS,GET,POST'"  # Allow methods as needed
     "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"  # Allow headers as needed
   }
+
+  depends_on = [aws_api_gateway_integration.mock_integration]  # Ensure this runs after the mock integration
 }
