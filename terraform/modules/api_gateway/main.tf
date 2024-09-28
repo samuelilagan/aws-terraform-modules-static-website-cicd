1f6e428fac6edc9e6f9e5642d3380df0aa6317cf
unknown <samuelilagan05@gmail.com>
2024-09-28 14:47:42 -0400

    fixed CORS issue in api_gateway module

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -17,12 +17,12 @@ resource "aws_api_gateway_method" "counter_method" {
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
-  resource_id = aws_api_gateway_resource.counter_resource.id
-  http_method = aws_api_gateway_method.counter_method.http_method
-  type        = "AWS_PROXY"
+  rest_api_id             = aws_api_gateway_rest_api.visitor_counter_api.id
+  resource_id             = aws_api_gateway_resource.counter_resource.id
+  http_method             = aws_api_gateway_method.counter_method.http_method
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"  
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
@@ -37,28 +37,39 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = split(":", var.lambda_function_arn)[6]  # Extract the function name from the ARN
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*"  
+  source_arn    = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*"
 }
 
-# Enable CORS
+# Enable CORS for OPTIONS method
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter_api.id
+  resource_id   = aws_api_gateway_resource.counter_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_method_response" "cors_method_response" {
   rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
   resource_id = aws_api_gateway_resource.counter_resource.id
-  http_method = aws_api_gateway_method.counter_method.http_method
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Origin"      = true
+    "method.response.header.Access-Control-Allow-Methods"    = true
+    "method.response.header.Access-Control-Allow-Headers"     = true
   }
 }
 
 resource "aws_api_gateway_integration_response" "cors_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.visitor_counter_api.id
   resource_id = aws_api_gateway_resource.counter_resource.id
-  http_method = aws_api_gateway_method.counter_method.http_method
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = aws_api_gateway_method_response.cors_method_response.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'"  # Or use your specific domain
+    "method.response.header.Access-Control-Allow-Methods"    = "'OPTIONS,GET,POST'"  # Allow methods as needed
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"  # Allow headers as needed
   }
 }
